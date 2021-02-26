8f6dbf1e216a00ac0ca554e767242d3bcafbb39e
ash3113 <ashchan.014@gmail.com>
2021-02-26 13:35:55 +0700

    first commit

---
+++ aktifitas1/aktifitas1.py
@@ -0,0 +1,10 @@
+#INPUT
+nama=input("Ashrida Salmadiina")
+umur=19
+
+#OUTPUT
+print("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PHYTON")
+print("nama: ", nama)
+print("umur: ", umur)
+print ("")
